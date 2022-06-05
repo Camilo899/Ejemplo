8263e98bb5efdc75e296fd88a0d77fe62453230a
Camilo899 <106891166+Camilo899@users.noreply.github.com>
2022-06-05 15:55:14 -0500

    Mensaje en Prueba

---
+++ Prueba.js
@@ -0,0 +1,10 @@
+/* Multiplicación
+Con JavaScript*/
+
+var numero_1 = 5;
+var numero_2 = 6;
+
+var operacion = numero_1 * numero_2;
+
+console.log("El resultado de la multiplicación es: " + operacion);
+console.log("Hola mi gente");
