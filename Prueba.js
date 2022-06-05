3f89cdd46ed72851cbcb8b283b7c61f74a002010
Camilo899 <106891166+Camilo899@users.noreply.github.com>
2022-06-05 15:58:34 -0500

    Cambiando operación en Pueba.js

--- Prueba.js
+++ Prueba.js
@@ -4,7 +4,7 @@ Con JavaScript*/
 var numero_1 = 5;
 var numero_2 = 6;
 
-var operacion = numero_1 * numero_2;
+var operacion = numero_1 + numero_2;
 
 console.log("El resultado de la multiplicación es: " + operacion);
 console.log("Hola mi gente");
